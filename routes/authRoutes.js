063fa6beeb5a3a3de06d6be5d0f0efec783560f5
kamranayesh <92720531+kamranayesh@users.noreply.github.com>
2024-03-28 01:29:49 +0530

    Create authRoutes.js

---
+++ routes/authRoutes.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const passport = require("passport");
+const { User, addUser, getUsers } = require("../models/user");
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await User.hashPassword(password);
+    const newUser = new User(getUsers().length + 1, username, hashedPassword);
+    addUser(newUser);
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/profile",
+    failureRedirect: "/auth/loginfail",
+    failureFlash: true,
+  })
+);
+
+router.get("/loginfail", (req, res) => {
+  res.status(402).json({ message: req.flash("error") });
+});
+
+router.get("/logout", (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/auth");
+  });
+});
+
+router.get("/", (req, res) => {
+  res.status(201).json({ message: "Logged out!" });
+});
+module.exports = router;
