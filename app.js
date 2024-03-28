db5bc7c22658647a987cb2b2ec1fa2a7681d2d48
kamranayesh <92720531+kamranayesh@users.noreply.github.com>
2024-03-28 21:27:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,19 +2,20 @@ let express = require("express");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const bcrypt = require("bcryptjs");
 const flash = require("connect-flash");
 const authRoutes = require("./routes/authRoutes");
 const profileRoutes = require("./routes/profileRoutes");
-const { getUsers } = require("./models/user");
+const { User, getUsers, addUser } = require("./models/user");
 
 const app = express();
-// Middleware
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
-    secret: "secret", // Change this to a more secure secret in production
+    secret: "secret",
     resave: true,
     saveUninitialized: true,
   })
@@ -23,7 +24,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
-// Passport local strategy configuration
 passport.use(
   new LocalStrategy(async (username, password, done) => {
     try {
@@ -41,6 +41,37 @@ passport.use(
   })
 );
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: "GOOGLE_CLIENT_ID", //replace with your google client id
+      clientSecret: "GOOGLE_CLIENT_SECRET", //replace with your google client secret
+      callbackURL: "http://localhost:3000/auth/google/callback", // Update with your callback URL
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await getUsers().findOne({ googleId: profile.id });
+
+        if (user) {
+          return done(null, user);
+        } else {
+          const newUser = new User({
+            googleId: profile.id,
+            username: profile.displayName,
+            email: profile.emails[0].value,
+          });
+          addUser(newUser);
+
+          await newUser.save();
+          return done(null, newUser);
+        }
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
+
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
@@ -50,11 +81,9 @@ passport.deserializeUser((id, done) => {
   done(null, user);
 });
 
-// Routes
 app.use("/auth", authRoutes);
 app.use("/profile", profileRoutes);
 
-// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
